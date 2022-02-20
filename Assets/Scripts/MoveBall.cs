d63bd5e60316f04b546924e53342f2fac959f97d
Adolfo León <50426680+Adogamm@users.noreply.github.com>
2022-02-19 19:10:01 -0600

    Primer commit

---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 10.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(horizontalInput, 0.0f, verticalInput) * speed);
+    }
+}
